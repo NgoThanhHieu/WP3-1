18fd42963eccb9f9d312cd9642f95ff7161800d9
NgoThanhHieu <66179085+NgoThanhHieu@users.noreply.github.com>
2021-02-17 14:03:51 +0100

    Add files via upload

---
+++ WP3-1/functions.php
@@ -0,0 +1,17 @@
+<?php
+
+function getCarInfo(int $year):void {
+
+ if ($year <= 1960) {
+ echo "To je fakt veterán!";
+} elseif ($year <= 1990) {
+echo "Auto by na silnici už raději být nemělo";
+} elseif ($year <= 2000) {
+ echo "Budete platit velkou silniční daň i povinné ručení";
+} elseif ($year <= 2010) {
+echo "Navštěvujte servis pravidelně";
+} else {
+echo "Auto je způsobilé";
+}
+
+}
